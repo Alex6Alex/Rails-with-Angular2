23c07ffa4d15af23434eaa2eebd8e97c2b3932eb
debuser <debuser@debian>
2017-02-05 20:36:31 +0300

    Update ymaps

--- public/app/components/pharmacies/pharmacies.component.js
+++ public/app/components/pharmacies/pharmacies.component.js
@@ -10,37 +10,96 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var pharmacy_service_1 = require('../../services/pharmacy.service');
+/// <reference path="ymaps.d.ts"/>
 var PharmaciesComponent = (function () {
-    function PharmaciesComponent(elementRef, pharmacyService) {
-        this.elementRef = elementRef;
+    function PharmaciesComponent(pharmacyService) {
         this.pharmacyService = pharmacyService;
         //change class for selected area
         this.area = 0;
+        this.pharms = [];
     }
-    PharmaciesComponent.prototype.ngAfterViewInit = function () {
-        /*ymaps initialzation*/
-        var ymaps = document.createElement("script");
-        ymaps.type = "text/javascript";
-        ymaps.src = "ymaps.js";
-        this.elementRef.nativeElement.appendChild(ymaps);
-        /*ymaps.ready().then(() => {
-            let myMap = new ymaps.Map("mymap", {
-                center: [55.76, 37.64],
-                zoom: 7
-            });
-
-            let myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-                hintContent: 'Москва!',
-                balloonContent: 'Столица России'
-            });
-        });*/
+    PharmaciesComponent.prototype.ngOnInit = function () {
+        var _this = this;
         this.getPharms();
+        ymaps.ready().then(function () {
+            _this.myMap = new ymaps.Map("mymap", {
+                center: [44.578526, 33.532156],
+                zoom: 11,
+                controls: ['zoomControl', 'fullscreenControl']
+            });
+            var HintLayout = ymaps.templateLayoutFactory.createClass("<div class='my-hint'>" +
+                "<b>{{ properties.title }}</b><br/> {{ properties.address }}" +
+                "</div>", {
+                getShape: function () {
+                    var el = this.getElement(), result = null;
+                    if (el) {
+                        var firstChild = el.firstChild;
+                        result = new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+                            [0, 0],
+                            [firstChild.offsetWidth, firstChild.offsetHeight]
+                        ]));
+                    }
+                    return result;
+                }
+            });
+            var _loop_1 = function(pharm) {
+                var myGeocoder = ymaps.geocode("\u0421\u0435\u0432\u0430\u0441\u0442\u043E\u043F\u043E\u043B\u044C, " + pharm.address);
+                myGeocoder.then(function (res) {
+                    var myPlacemark = new ymaps.Placemark(res.geoObjects.get(0).geometry.getCoordinates(), {
+                        title: pharm.name,
+                        address: pharm.address
+                    }, {
+                        hintLayout: HintLayout,
+                        preset: 'islands#darkGreenMedicalIcon'
+                    });
+                    _this.myMap.geoObjects.add(myPlacemark);
+                });
+            };
+            for (var _i = 0, _a = _this.pharms; _i < _a.length; _i++) {
+                var pharm = _a[_i];
+                _loop_1(pharm);
+            }
+        });
     };
     PharmaciesComponent.prototype.getPharms = function () {
         var _this = this;
         this.pharmacyService.getPharms().subscribe(function (data) {
             _this.pharms = data;
         });
+    };
+    PharmaciesComponent.prototype.setArea = function (num) {
+        switch (num) {
+            case 0: {
+                this.area = 0;
+                this.myMap.setCenter([44.578526, 33.532156]);
+                this.myMap.setZoom(11);
+                break;
+            }
+            case 1: {
+                this.area = 1;
+                this.myMap.setCenter([44.568588, 33.452416]);
+                this.myMap.setZoom(13);
+                break;
+            }
+            case 2: {
+                this.area = 2;
+                this.myMap.setCenter([44.584961, 33.524793]);
+                this.myMap.setZoom(13);
+                break;
+            }
+            case 3: {
+                this.area = 3;
+                this.myMap.setCenter([44.615463, 33.568546]);
+                this.myMap.setZoom(13);
+                break;
+            }
+            case 4: {
+                this.area = 4;
+                this.myMap.setCenter([44.528813, 33.594336]);
+                this.myMap.setZoom(13);
+                break;
+            }
+        }
     };
     PharmaciesComponent = __decorate([
         core_1.Component({
@@ -49,7 +108,7 @@ var PharmaciesComponent = (function () {
             templateUrl: 'pharmacies.component.html',
             styleUrls: ['pharmacies.css']
         }), 
-        __metadata('design:paramtypes', [core_1.ElementRef, pharmacy_service_1.PharmacyService])
+        __metadata('design:paramtypes', [pharmacy_service_1.PharmacyService])
     ], PharmaciesComponent);
     return PharmaciesComponent;
 }());
--- public/app/components/pharmacies/pharmacies.component.ts
+++ public/app/components/pharmacies/pharmacies.component.ts
@@ -1,6 +1,7 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PharmacyService } from '../../services/pharmacy.service';
+/// <reference path="ymaps.d.ts"/>
 
 @Component({
 	moduleId: module.id,
@@ -9,36 +10,101 @@ import { PharmacyService } from '../../services/pharmacy.service';
 	styleUrls: ['pharmacies.css']
 })
 
-export class PharmaciesComponent implements AfterViewInit {
+export class PharmaciesComponent implements OnInit {
 	//change class for selected area
 	area = 0;
-	pharms: Array<Object>;
+	pharms = [];
+	myMap;
 
-	constructor(private elementRef: ElementRef, private pharmacyService: PharmacyService){}
+	constructor(private pharmacyService: PharmacyService){}
+	
 
-	ngAfterViewInit(): void{
-		/*ymaps initialzation*/
-		let ymaps = document.createElement("script");
-		ymaps.type = "text/javascript";
-		ymaps.src = "ymaps.js";
-		this.elementRef.nativeElement.appendChild(ymaps);
-		/*ymaps.ready().then(() => {
-			let myMap = new ymaps.Map("mymap", {
-				center: [55.76, 37.64],
-		        zoom: 7
+	ngOnInit(): void{
+
+		this.getPharms();
+
+		ymaps.ready().then(() => {
+			this.myMap = new ymaps.Map("mymap", {
+				center: [44.578526, 33.532156],
+		        zoom: 11,
+		        controls: ['zoomControl', 'fullscreenControl']
 			});
 
-			let myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-		        hintContent: 'Москва!',
-		        balloonContent: 'Столица России'
-		    });
-		});*/
-		this.getPharms();
+			let HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='my-hint'>" +
+		    "<b>{{ properties.title }}</b><br/> {{ properties.address }}" +
+		    "</div>", {
+		            getShape: function () {
+		                var el = this.getElement(),
+		                    result = null;
+		                if (el) {
+		                    var firstChild = el.firstChild;
+		                    result = new ymaps.shape.Rectangle(
+		                        new ymaps.geometry.pixel.Rectangle([
+		                            [0, 0],
+		                            [firstChild.offsetWidth, firstChild.offsetHeight]
+		                        ])
+		                    );
+		                }
+		                return result;
+		            }
+		        }
+		    );
+
+		    for (let pharm of this.pharms){
+				let myGeocoder = ymaps.geocode(`Севастополь, ${pharm.address}`);
+			    myGeocoder.then(res => {
+			    	let myPlacemark = new ymaps.Placemark(res.geoObjects.get(0).geometry.getCoordinates(), {
+		                title: pharm.name,
+		                address: pharm.address
+		            }, {
+		                hintLayout: HintLayout,
+		                preset: 'islands#darkGreenMedicalIcon'
+		            });
+		            
+		            this.myMap.geoObjects.add(myPlacemark);
+			    });
+			}
+		});
 	}
 
 	getPharms(){
 		this.pharmacyService.getPharms().subscribe(data => {
 			this.pharms = data;
 		});
+	}
+
+	setArea(num: number){
+		switch(num){
+	        case 0:{
+	        	this.area = 0;
+	            this.myMap.setCenter([44.578526, 33.532156]);
+	            this.myMap.setZoom(11);
+	            break;
+	        }
+	        case 1:{
+	        	this.area = 1;
+	            this.myMap.setCenter([44.568588, 33.452416]);
+	            this.myMap.setZoom(13);
+	            break;
+	        }
+	        case 2:{
+	        	this.area = 2;
+	            this.myMap.setCenter([44.584961, 33.524793]);
+	            this.myMap.setZoom(13);
+	            break;
+	        }
+	        case 3:{
+	        	this.area = 3;
+	            this.myMap.setCenter([44.615463, 33.568546]);
+	            this.myMap.setZoom(13);
+	            break;
+	        }
+	        case 4:{
+	        	this.area = 4;
+	            this.myMap.setCenter([44.528813, 33.594336]);
+	            this.myMap.setZoom(13);
+	            break;
+	        }
+	    }
 	}
 }
