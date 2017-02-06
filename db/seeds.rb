9d08c572eafeeee2db0de07c0e409489be00bc99
debuser <debuser@debian>
2017-02-06 22:40:59 +0300

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pharmacy.create(name: 'Аптека №1', address: 'Восстасших, 1', phone: '54-44-65', worktime: 'круглосуточно', area: 'Ленинский')
+Pharmacy.create(name: 'АКиВ', address: 'Нахимова, 10', phone: '65-34-21', worktime: '8.00 - 19.00', area: 'Ленинский')
+Pharmacy.create(name: 'АКВ', address: 'Сталинграда, 28', phone: '26-45-77', worktime: '7.00 - 19.00', area: 'Гагаринский')
+Pharmacy.create(name: 'Аптека №23', address: 'Ульянова, 2', phone: '24-25-87', worktime: 'круглосуточно', area: 'Гагаринский')
+Pharmacy.create(name: 'АВиК', address: 'Героев Севастополя, 25', phone: '79-97-12', worktime: 'круглосуточно', area: 'Нахимовский')
+Pharmacy.create(name: 'Аптека №1', address: 'Захарова, 4', phone: '97-38-29', worktime: 'круглосуточно', area: 'Нахимовский')
+Pharmacy.create(name: 'Добрый доктор', address: '1 Мая, 1', phone: '84-44-88', worktime: 'круглосуточно', area: 'Балаклавский')
+Pharmacy.create(name: 'АВК', address: 'Новикова, 28', phone: '84-96-65', worktime: '7.00 - 21.00', area: 'Балаклавский')
