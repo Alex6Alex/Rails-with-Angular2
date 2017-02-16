86e700ab7da9bf7d4067ee05c6bbf2134659bac3
debuser <debuser@debian>
2017-02-16 23:16:04 +0300

    11

---
+++ public/app/components/pharmacies/ymaps.d.ts
@@ -0,0 +1,18 @@
+declare var ymaps: any;
+/*declare namespace ymaps {
+  export function ready(): Promise;
+
+  class Promise {
+    then(onFulfilled?: Function, onRejected?: Function, onProgress?: Function, ctx?: any): Promise;
+  }
+
+  export class Map {
+    constructor(element: string | any, state: MapState);
+  }
+
+  export class MapState {
+    center: number[];
+    controls: string[];
+    zoom: number;
+  }
+}*/
