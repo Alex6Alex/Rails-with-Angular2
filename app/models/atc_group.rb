979779f3b8d1375441d17ef94084490ee6382472
debuser <debuser@debian>
2017-02-26 12:30:51 +0300

    add medicines

--- app/models/atc_group.rb
+++ app/models/atc_group.rb
@@ -2,5 +2,5 @@ class AtcGroup < ApplicationRecord
 	validates :code, presence: true, 
 			uniqueness: { case_sensitive: false }
 
-	#has_many :atcSubGroups, dependent: :destroy
+	has_many :atcSubGroups, dependent: :destroy
 end
